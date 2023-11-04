7c5eb431b5c27154295276671da91caa4a3bf8fa
Олександр Канюка <crusaider423@gmail.com>
2023-11-04 17:38:55 +0200

    test

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(
+    e =>
+      localStorage.setItem(
+        'videoplayer-current-time',
+        JSON.stringify(e.seconds)
+      ),
+    1000
+  )
+);
+
+const storageTimePars = localStorage.getItem('videoplayer-current-time');
+
+if (storageTimePars !== null) {
+  player.setCurrentTime(JSON.parse(storageTimePars));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onSubmit);
+
+function onFormInput(e) {
+  console.log(e);
+  if (e.currentTarget === null) {
+    return;
+  }
+  const email = e.currentTarget.elements.email.value;
+  const message = e.currentTarget.elements.message.value;
+
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email, message })
+  );
+}
+
+const locStorGet = localStorage.getItem('feedback-form-state');
+
+if (locStorGet !== null) {
+  const locStorPars = JSON.parse(locStorGet);
+  formRef.elements.email.value = locStorPars.email;
+  formRef.elements.message.value = locStorPars.message;
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if (!(e.target.elements.email.value && e.target.elements.message.value)) {
+    return;
+  }
+  console.log(JSON.parse(locStorGet));
+  e.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}
